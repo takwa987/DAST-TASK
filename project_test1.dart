96467f94962b41e612aa78d6f828cfa1d7535a1e
takwa987 <alomaritakwa51@gmail.com>
2025-10-20 10:25:19 -0700

    Add files via upload

---
+++ project_test1.dart
@@ -0,0 +1,73 @@
+import 'dart:io';//i=input,o=output 
+void main(){
+
+
+
+      //  int age=20;
+
+        //Z=3;
+
+const double pi=3.14;
+
+final timeTologin = DateTime.now();
+print(timeTologin);
+ 
+int? x=9;
+print(x);//null
+//input from user
+//print (from user)
+stdout.write("please enter your name :");  //write
+String name = stdin.readLineSync()!; //String
+print(name);
+stdout.write("please enter yoyr age:");
+//parse
+int age=int.parse(stdin.readLineSync()!);
+ int num=8;
+ int num2=2;
+ print(num/num2);
+
+ //add
+ //Print(++num2);//7
+ //Print(num++);//2
+//Print(num);//3
+num2++;
+print(num2);//7
+num2+=2;
+print(num2);
+
+ 
+ 
+ 
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+ 
+ 
+
+
+
+
+ //add
+
+
+
+ 
+
+
+
+
+
+
+}
+
+
+
